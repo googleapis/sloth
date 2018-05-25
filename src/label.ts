3dda184612418a559859a024c4f200f1c374fed3
Justin Beckwith <beckwith@google.com>
2018-05-24 22:07:14 -0700

    cleanup labels

--- src/label.ts
+++ src/label.ts
@@ -14,48 +14,49 @@ const octo = new Octokit();
 octo.authenticate({token, type: 'token'});
 
 export async function reconcileLabels() {
-  const owner = 'google';
-  const repo = 'google-api-nodejs-client';
-  const res = await octo.issues.getLabels({owner, repo});
-  const oldLabels = res.data as Label[];
-  console.log(`Found ${oldLabels.length} existing tags.`);
   const promises = new Array<Promise<Octokit.AnyResponse|void>>();
-  labels.forEach(l => {
-    // try to find a label with the same name
-    const match = oldLabels.find(x => x.name === l.name);
-    if (match) {
-      // check to see if the color matches
-      console.log(`Found a match for ${match.name}.`);
-      if (match.color !== l.color) {
-        console.log(`Updating color for ${match.name}.`);
+  repos.forEach(async (r) => {
+    const [owner, repo] = r.repo.split('/');
+    const res = await octo.issues.getLabels({owner, repo, per_page: 100});
+    const oldLabels = res.data as Label[];
+    labels.forEach(l => {
+      // try to find a label with the same name
+      const match = oldLabels.find(x => x.name === l.name);
+      if (match) {
+        // check to see if the color matches
+        if (match.color !== l.color) {
+          console.log(`Updating color for ${match.name} from ${match.color} to ${l.color}.`);
+          const p = octo.issues
+                        .updateLabel({
+                          repo,
+                          owner,
+                          name: l.name,
+                          oldname: l.name,
+                          description: match.description,
+                          color: l.color
+                        })
+                        .catch(e => {
+                          console.error(`Error updating label ${l.name} in ${owner}/${repo}`);
+                        });
+          promises.push(p);
+        }
+      } else {
+        // there was no match, go ahead and add it
+        console.log(`Creating label for ${l.name}.`);
         const p = octo.issues
-                      .updateLabel({
+                      .createLabel({
                         repo,
                         owner,
-                        name: l.name,
-                        oldname: l.name,
-                        description: match.description,
-                        color: l.color.slice(1)
+                        color: l.color,
+                        description: l.description,
+                        name: l.name
                       })
-                      .catch(console.error);
+                      .catch(e => {
+                        console.error(`Error creating label ${l.name} in ${owner}/${repo}`);
+                      });
         promises.push(p);
-      } else {
-        console.log(`Color for ${match.name} was correct.`);
       }
-    } else {
-      // there was no match, go ahead and add it
-      console.log(`Creating label for ${l.name}.`);
-      const p = octo.issues
-                    .createLabel({
-                      repo,
-                      owner,
-                      color: l.color.slice(1),
-                      description: l.description,
-                      name: l.name
-                    })
-                    .catch(console.error);
-      promises.push(p);
-    }
+    });
   });
   await Promise.all(promises);
 }
