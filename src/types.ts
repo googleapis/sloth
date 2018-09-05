343ceb6796d0e942e35f0ae59560dbd86029d5f5
danoscarmike <omeara.dan@gmail.com>
2018-09-05 15:15:51 -0700

    Revert "add language attribute to RepoResults"
    This reverts commit eea41aa174548171491b4e8edea7b7f4ebe57d0a.

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ReposUpdateBranchProtectionParamsRequiredStatusChecks } from '@octokit/rest';
+import {ReposUpdateBranchProtectionParamsRequiredStatusChecks} from '@octokit/rest';
 
 export interface IssueResult {
   issues: Issue[];
@@ -29,42 +29,42 @@ export interface Issue {
   repo: string;
   owner: string;
   url:
-  string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22',
+      string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22',
   repository_url:
-  string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner',
+      string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner',
   labels_url:
-  string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/labels{/name}',
+      string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/labels{/name}',
   comments_url:
-  string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/comments',
+      string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/comments',
   events_url:
-  string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/events',
+      string;  // 'https://api.github.com/repos/googleapis/nodejs-spanner/issues/22/events',
   html_url:
-  string;       // 'https://github.com/googleapis/nodejs-spanner/issues/22',
+      string;       // 'https://github.com/googleapis/nodejs-spanner/issues/22',
   id: number;       // 269235010,
   number: number;   // 22,
   title: string;    // 'Cloud Spanner documentation should talk about supported
-  // types',
+                    // types',
   user: {};         // [Object],
   labels: Label[];  // [Array],
   state: string;    // 'open',
   locked: boolean;  // false,
-  assignee: string | null;       // null,
+  assignee: string|null;       // null,
   assignees: string[];         // [],
-  milestone: string | null;      // null,
+  milestone: string|null;      // null,
   comments: number;            // 2,
   created_at: string;          // '2017-10-27T21:04:49Z',
   updated_at: string;          // '2018-02-20T21:03:44Z',
   closed_at: string;           // null,
   author_association: string;  // 'MEMBER',
   body: string;   // '_From @vkedia on October 4, 2017 22:33_\n\nOn the cloud
-  // spanner documentation, I could not find any mention of what
-  // data types are supported and how are these to be specified
-  // in the client library. For eg how would I specify a date or
-  // a timestamp or a float or bytes. I have seen customers
-  // specifying bytes as Base64 encoded since the rpc protocol
-  // talks about that. But the client library does Base64
-  // encoding on their behalf.\n\n_Copied from original issue:
-  // GoogleCloudPlatform/google-cloud-node#2654_',
+                  // spanner documentation, I could not find any mention of what
+                  // data types are supported and how are these to be specified
+                  // in the client library. For eg how would I specify a date or
+                  // a timestamp or a float or bytes. I have seen customers
+                  // specifying bytes as Base64 encoded since the rpc protocol
+                  // talks about that. But the client library does Base64
+                  // encoding on their behalf.\n\n_Copied from original issue:
+                  // GoogleCloudPlatform/google-cloud-node#2654_',
   reactions: {};  // [Object]
   pull_request: {};
   isTriaged: boolean;
@@ -83,7 +83,6 @@ export interface Label {
 
 export interface RepoResult extends Result {
   repo: string;
-  repoLanguage: string;
 }
 
 export interface LanguageResult extends Result {
@@ -124,7 +123,7 @@ export interface Team {
   privacy: string;      //'closed',
   url: string;          //'https://api.github.com/teams/286166',
   members_url:
-  string;  //'https://api.github.com/teams/286166/members{/member}',
+      string;  //'https://api.github.com/teams/286166/members{/member}',
   repositories_url: string;  //'https://api.github.com/teams/286166/repos',
   permission: string;        //'push',
   parent: string;            // null
@@ -141,9 +140,9 @@ export interface Organization {
   hooks_url: string;   // "https://api.github.com/orgs/github/hooks",
   issues_url: string;  // "https://api.github.com/orgs/github/issues",
   members_url:
-  string;  // "https://api.github.com/orgs/github/members{/member}",
+      string;  // "https://api.github.com/orgs/github/members{/member}",
   public_members_url:
-  string;  // "https://api.github.com/orgs/github/public_members{/member}",
+      string;  // "https://api.github.com/orgs/github/public_members{/member}",
   avatar_url: string;   // "https://github.com/images/error/octocat_happy.gif",
   description: string;  // "A great organization"
 }
