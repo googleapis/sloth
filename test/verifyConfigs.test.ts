77fa7c3ef5920202d04554641891f18e94ee04e5
Jonathan Lui <jonathanlui@google.com>
2018-11-20 14:38:20 -0800

    validate repos.json schema

--- test/verifyConfigs.test.ts
+++ test/verifyConfigs.test.ts
@@ -6,37 +6,57 @@ const REPOS_JSON = path.join(__dirname, '../../repos.json');
 const USERS_JSON = path.join(__dirname, '../../users.json');
 
 describe('Verify config files', () => {
-  it('repos.json is valid json', () => {
-    const file = fs.readFileSync(REPOS_JSON, 'utf-8');
-    assert.doesNotThrow(() => JSON.parse(file));
+  describe('repos.json', () => {
+    it('is valid json', () => {
+      const file = fs.readFileSync(REPOS_JSON, 'utf-8');
+      assert.doesNotThrow(() => JSON.parse(file));
+    });
+
+    it('has valid schema', () => {
+      // tslint:disable-next-line:no-any
+      const assertRepo = (entry: any) => {
+        assert('string', typeof entry.repo);
+        assert('string', typeof entry.language);
+      };
+
+      const file = fs.readFileSync(REPOS_JSON, 'utf-8');
+      const repos = JSON.parse(file);
+
+      assert('object', repos);
+      assert(Array.isArray(repos.repos));
+
+      repos.repos.forEach(assertRepo);
+    });
   });
 
-  it('users.json is valid json', () => {
-    const file = fs.readFileSync(USERS_JSON, 'utf-8');
-    assert.doesNotThrow(() => JSON.parse(file));
-  });
+  describe('users.json', () => {
+    it('is valid json', () => {
+      const file = fs.readFileSync(USERS_JSON, 'utf-8');
+      assert.doesNotThrow(() => JSON.parse(file));
+    });
 
-  it('users.json has valid schema', () => {
-    // tslint:disable-next-line:no-any
-    const assertArrayOfStrings = (array: any) => assert(
-        Array.isArray(array) &&
-        array.every((elem) => typeof elem === 'string'));
+    it('has valid schema', () => {
+      // tslint:disable-next-line:no-any
+      const assertArrayOfStrings = (array: any) => assert(
+          Array.isArray(array) &&
+          array.every((elem) => typeof elem === 'string'));
 
-    // tslint:disable-next-line:no-any
-    const assertMembership = (membership: any) => {
-      assert('string', typeof membership.team);
-      assertArrayOfStrings(membership.users);
-      assertArrayOfStrings(membership.repos);
-    };
+      // tslint:disable-next-line:no-any
+      const assertMembership = (membership: any) => {
+        assert('string', typeof membership.team);
+        assertArrayOfStrings(membership.users);
+        assertArrayOfStrings(membership.repos);
+      };
 
-    const file = fs.readFileSync(USERS_JSON, 'utf-8');
-    const users = JSON.parse(file);
+      const file = fs.readFileSync(USERS_JSON, 'utf-8');
+      const users = JSON.parse(file);
 
-    assert('object', typeof users);
+      assert('object', typeof users);
 
-    assertArrayOfStrings(users.orgs);
+      assertArrayOfStrings(users.orgs);
 
-    assert(Array.isArray(users.membership));
-    users.membership.forEach(assertMembership);
+      assert(Array.isArray(users.membership));
+      users.membership.forEach(assertMembership);
+    });
   });
 });
