77af3955fdb7953157115c742862d567e0c00488
Justin Beckwith <beckwith@google.com>
2019-12-15 13:05:12 -0800

    feat!: remove labels command

--- src/util.ts
+++ src/util.ts
@@ -17,7 +17,6 @@ import * as Octokit from '@octokit/rest';
 import {Label, Repo, Users, Team} from './types';
 
 export const repos: Repo[] = require('../../repos.json').repos;
-export const labels: Label[] = require('../../labels.json').labels;
 export const users: Users = require('../../users.json');
 export const teams: Team[] = require('../../teams.json').teams;
 export const languages = [
