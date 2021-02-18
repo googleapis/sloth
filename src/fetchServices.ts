6c76bfa5a6089c76275051e4ff16dacd8ecfcb88
Dan O'Meara <omeara.dan@gmail.com>
2021-02-18 12:00:24 -0800

    chore: uses proper types
    removes unnecessary type annotations
    
    removes try/catch wrappers

--- src/fetchServices.ts
+++ src/fetchServices.ts
@@ -14,35 +14,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {google} from 'googleapis';
+import {google, servicemanagement_v1} from 'googleapis';
 const spreadsheetId = '14JYzl_8W3HyD0c1jYDXQJA2sVWBZiMoxT8Sp57xGWvk';
 
 /**
  * Make a call to the Google Cloud Service Management API's services.list endpoint
- * @return {string[]} an array of hostnames ('foo.googleapis.com') for all available services
+ * @return an array of hostnames ('foo.googleapis.com') for all available services
  */
 export async function getAllServiceNames(): Promise<string[]> {
-  try {
-    const auth = await google.auth.getClient({
-      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-    const client = google.servicemanagement({
-      version: 'v1',
-      auth: auth,
-    });
-    const serviceNames: string[] = [];
-    return await listServices(client, serviceNames);
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  });
+  const client = google.servicemanagement({
+    version: 'v1',
+    auth: auth,
+  });
+  const serviceNames: string[] = [];
+  return await listServices(client, serviceNames);
 }
 
 export async function listServices(
-  client: any,
+  client: servicemanagement_v1.Servicemanagement,
   serviceNames: string[],
   nextPageToken = ''
-): Promise<any> {
+): Promise<string[]> {
   const res = await client.services.list(
     {
       pageSize: 100,
@@ -56,8 +51,10 @@ export async function listServices(
   );
 
   if (res.data.services) {
-    res.data.services.map((item: any) => {
-      serviceNames.push(item.serviceName);
+    res.data.services.map(item => {
+      if (item.serviceName) {
+        serviceNames.push(item.serviceName);
+      }
     });
   }
 
@@ -69,107 +66,86 @@ export async function listServices(
 
 /**
  * Make a call to the Google Cloud Service Management API's services.getConfig endpoint
- * @param {string} serviceName - the hostname of a service ('foo.googleapis.com')
- * @return {Object} the service config of the passed hostname
+ * @param serviceName - the hostname of a service ('foo.googleapis.com')
+ * @return the service config of the passed hostname
  */
-export async function getServiceConfig(serviceName: string): Promise<any> {
-  try {
-    const auth = await google.auth.getClient({
-      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-    const servicemanagement = google.servicemanagement({
-      version: 'v1',
-      auth: auth,
-    });
-    const res = await servicemanagement.services.getConfig(
-      {
-        serviceName: serviceName,
+export async function getServiceConfig(
+  serviceName: string
+): Promise<servicemanagement_v1.Schema$Service> {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  });
+  const servicemanagement = google.servicemanagement({
+    version: 'v1',
+    auth: auth,
+  });
+  const res = await servicemanagement.services.getConfig(
+    {
+      serviceName: serviceName,
+    },
+    {
+      retryConfig: {
+        retry: 10,
       },
-      {
-        retryConfig: {
-          retry: 10,
-        },
-      }
-    );
-    return res.data;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+    }
+  );
+  return res.data;
 }
 
 /**
  * Get service configs for all available services.
- * @return {Object}
+ * @return the service configuration object
  */
-export async function getAllServiceConfigs(): Promise<any> {
-  try {
-    const serviceNames = await getAllServiceNames();
-    const serviceConfigs = serviceNames.map(async (name: string) => {
-      await getServiceConfig(name);
-    });
-    return serviceConfigs;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+export async function getAllServiceConfigs() {
+  const serviceNames = await getAllServiceNames();
+  const serviceConfigs = serviceNames.map(async (name: string) => {
+    await getServiceConfig(name);
+  });
+  return serviceConfigs;
 }
 
 /**
  * Determines if a given service is a 'Cloud API'.
- * @param {string} serviceName - the hostname of a service ('foo.googleapis.com')
- * @return {Boolean}
+ * @param serviceName - the hostname of a service ('foo.googleapis.com')
+ * @return well is it, or not?
  */
 export async function isCloudApi(serviceName: string): Promise<boolean> {
-  try {
-    const serviceConfig = await getServiceConfig(serviceName);
+  const serviceConfig = await getServiceConfig(serviceName);
 
-    if ('title' in serviceConfig) {
-      if (serviceConfig.title.includes('Firebase')) {
-        return false;
-      }
-    }
-    if ('authentication' in serviceConfig) {
-      if ('rules' in serviceConfig['authentication']) {
-        const scopes: boolean[] = serviceConfig.authentication.rules.map(
-          (item: any) => {
-            if ('oauth' in item) {
-              if ('canonicalScopes' in item['oauth']) {
-                const scopesStr: string = item.oauth.canonicalScopes;
-                if (scopesStr.includes('auth/firebase')) {
-                  return false;
-                } else if (scopesStr.includes('auth/cloud-platform')) {
-                  return true;
-                } else if (scopesStr.includes('auth/drive')) {
-                  return true;
-                } else if (scopesStr.includes('auth/apps')) {
-                  return true;
-                }
-              }
-            }
-            return false;
-          }
-        );
-
-        if (scopes.includes(true)) {
+  if (serviceConfig.title?.includes('Firebase')) {
+    return false;
+  }
+  if (serviceConfig.authentication?.rules) {
+    const scopes: boolean[] = serviceConfig.authentication.rules.map(item => {
+      if (item.oauth?.canonicalScopes) {
+        const scopesStr: string = item.oauth.canonicalScopes;
+        if (scopesStr.includes('auth/firebase')) {
+          return false;
+        } else if (scopesStr.includes('auth/cloud-platform')) {
+          return true;
+        } else if (scopesStr.includes('auth/drive')) {
+          return true;
+        } else if (scopesStr.includes('auth/apps')) {
           return true;
         }
       }
-    }
+      return false;
+    });
 
-    // usage: { requirements: [ 'serviceusage.googleapis.com/tos/cloud' ] }
-    if (serviceConfig.usage.requirements) {
-      const usage: string[] = serviceConfig.usage.requirements;
-      if (usage.includes('serviceusage.googleapis.com/tos/cloud')) {
-        return true;
-      }
+    if (scopes.includes(true)) {
+      return true;
     }
-
-    return false;
-  } catch (e) {
-    console.log(e);
-    throw e;
   }
+
+  // usage: { requirements: [ 'serviceusage.googleapis.com/tos/cloud' ] }
+  if (serviceConfig.usage?.requirements) {
+    const usage: string[] = serviceConfig.usage.requirements;
+    if (usage.includes('serviceusage.googleapis.com/tos/cloud')) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**
