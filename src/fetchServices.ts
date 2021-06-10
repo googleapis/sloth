edb94e16a703c8df8cce1dbfc13f0e294d9e6da9
Justin Beckwith <justin.beckwith@gmail.com>
2021-06-10 11:15:42 -0700

    refactor: use split apiary module (#955)

--- src/fetchServices.ts
+++ src/fetchServices.ts
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {google} from 'googleapis';
+import * as google from '@googleapis/sheets';
 import {ServiceManagerClient, protos} from '@google-cloud/service-management';
 import * as Throttle from 'p-throttle';
 import * as meow from 'meow';
